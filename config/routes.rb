d97568b9c0cfdd252a819439690336aa9ee10690
Grégoire DIEF <gregoiredief@gmail.com>
2018-11-26 17:48:38 +0100

    The routes are here, and i began the potagerController

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  resources :potagers, only: [ :new, :create, :show] do
+    resources :choices, only: [ :new, :create ]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
