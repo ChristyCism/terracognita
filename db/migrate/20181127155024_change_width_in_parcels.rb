40c29b5c71352db69bf09ec57136dfceec5aa47d
Annabelle PELLIER <annabplr@gmail.com>
2018-11-27 16:58:18 +0100

    edit name parcel in the migration

--- db/migrate/20181127155024_change_width_in_parcels.rb
+++ db/migrate/20181127155024_change_width_in_parcels.rb
@@ -1,6 +1,6 @@
 class ChangeWidthInParcels < ActiveRecord::Migration[5.2]
   def change
-    rename_column :parcel, :width, :order_from_south
-    rename_column :parcel, :length, :size
+    rename_column :parcels, :width, :order_from_south
+    rename_column :parcels, :length, :size
   end
 end
