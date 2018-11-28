16886489dc44adc2413b44f81191b3fb2640db51
Annabelle PELLIER <annabplr@gmail.com>
2018-11-28 15:46:06 +0100

    animation when selecting a vegetable

--- app/javascript/components/choices.js
+++ app/javascript/components/choices.js
@@ -2,16 +2,16 @@
 // init the state from the input
 $(".image-checkbox").each(function () {
   if ($(this).find('input[type="checkbox"]').first().attr("checked")) {
-    $(this).addClass('image-checkbox-checked');
+    $(this).addClass('shadow-pop-bl');
   }
   else {
-    $(this).removeClass('image-checkbox-checked');
+    $(this).removeClass('shadow-pop-bl');
   }
 });
 
 // sync the state to the input
 $(".image-checkbox").on("click", function (e) {
-  $(this).toggleClass('image-checkbox-checked');
+  $(this).toggleClass('shadow-pop-bl');
   var $checkbox = $(this).find('input[type="checkbox"]');
   $checkbox.prop("checked",!$checkbox.prop("checked"))
 
