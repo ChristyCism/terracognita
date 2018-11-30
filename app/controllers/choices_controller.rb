cb49f7e6869b6161e0fd6311c8f8815300906357
Christy <valette.christy@gmail.com>
2018-11-30 12:11:28 +0100

    avec Kévin

--- app/controllers/choices_controller.rb
+++ app/controllers/choices_controller.rb
@@ -2,20 +2,22 @@ class ChoicesController < ApplicationController
   skip_before_action :authenticate_user!
 
   def new
-    @choice = Choice.new
+    @potager = Potager.find(params[:potager_id])
     @vegetables = Vegetable.where("'Mai'=ANY(month_planted)")
-
   end
 
   def create
     @new_choice = Choice.new(params_of_choice)
-    @new_choice.save
+    p @new_choice
+
+
     if @new_choice.save
       redirect_to potager_path(:potager_id)
     else
-      redirect_to potager_path(:potager_id)
+      render_errors
     end
   end
+
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :potagers, only: [ :new, :create, :show] do
+  resources :potagers, only: [ :new, :create, :show, :update] do
     resources :choices, only: [ :new, :create ]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
