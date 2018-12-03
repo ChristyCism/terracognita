35bdebfcf9df25d040d5a180fc304ed617e8e1cc
Philippe Morfin <morfinphilippe@gmail.com>
2018-12-03 14:04:37 +0100

    Checked and cleaned up a few methods

--- app/controllers/choices_controller.rb
+++ app/controllers/choices_controller.rb
@@ -6,22 +6,4 @@ class ChoicesController < ApplicationController
     @vegetables = Vegetable.where("'Mai'=ANY(month_planted)")
   end
 
-  def create
-    @new_choice = Choice.new(params_of_choice)
-    p @new_choice
-
-
-    if @new_choice.save
-      redirect_to potager_path(:potager_id)
-    else
-      render_errors
-    end
-  end
-
-
-  private
-
-  def params_of_choice
-    params.require(:choice).permit(:vegetable_id, :potager_id)
-  end
 end
